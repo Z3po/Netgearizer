9e788f74085fb34150c081b1b1d8902909b6401d
Sebastian Cabrera <scabrera-github@portachtzig.de>
2012-03-26 00:29:36 +0200

    resultparser is now parsing resultsets. repaired discovery function

--- netgearswitch.py
+++ netgearswitch.py
@@ -28,7 +28,8 @@ def get_mac():
 
 # global hex data
 sequence = '00000001'
-macaddress = get_mac()
+mymac = get_mac()
+destmac = '000000000000'
 
 # initialize a socket
 # SOCK_DGRAM specifies that this is UDP
@@ -53,7 +54,7 @@ def send_data(senddata):
     except socket.timeout:
         print 'SOCKET TIMED OUT'
 
-def constructdata(sequence,data,reqtype='0101',mymac='0',destmac='0'):
+def constructdata(sequence,adddata,reqtype='0101',mymac='0',destmac='0'):
     nsdpnoerror = '000000000000'
     nsdpseperator = '00000000'
     enddata = 'ffff0000'
@@ -61,79 +62,75 @@ def constructdata(sequence,data,reqtype='0101',mymac='0',destmac='0'):
     data = reqtype 
     data += nsdpnoerror + mymac.rjust(12,'0') + destmac.rjust(12,'0') 
     data += sequence + nsdpheader + nsdpseperator 
-    data += data
+    data += adddata
     data += enddata
     return data
 
+def parsedata(sequence,data):
+    nsdpnoerror = '000000000000'
+    dataresult = {}
+    dataresult.update({ 'packettype' : data[:4] })
+    data = data[4:]
+    if data[:12] == nsdpnoerror:
+        data = data[36:]
+        if data[:8] == sequence:
+            data = data[24:]
+            while data[:8] != 'ffff0000':
+                length = 2*int('0x'+data[4:8],0)
+                dataresult.update({ data[:4] : data[8:(8+length)] })
+                data = data[(8+length):]
+        else:
+            print 'Not a result to my sequence!'
+    else:
+        print 'Error! ' + data[:12]
+    return dataresult
+
+
 
 def do_discovery():
-    global sequence
+    global sequence, destmac
     discoveryheader = '0101'
     discoveryansheader = '0102'
-    switchtypestart = '00010008'
-    switchtypeend = '0002'
-    switchfirmverstart = '000d0007'
-    switchfirmverend = '000e0000'
+    switchtype = '0001'
+    switchname = '0003'
+    switchmac = '0004'
+    switchip = '0006'
+    switchnetmask = '0007'
+    switchgateway = '0008'
+    switchfirmver = '000d'
     
-    discoverydata = constructdata(sequence,'0001000000020000000300000004000000050000000600000007000000080000000b0000000c0000000d0000000e0000000f000074000000',discoveryheader,macaddress)
+    discoverydata = constructdata(sequence,'0001000000020000000300000004000000050000000600000007000000080000000b0000000c0000000d0000000e0000000f000074000000',discoveryheader,mymac,destmac)
     discoveryresult = send_data(binascii.unhexlify(discoverydata))
     if discoveryresult != None:
         result = binascii.hexlify(discoveryresult)
-        if result[:4] == discoveryansheader:
-            print 'got a successfull discovery'
-            result = result[4:]
-            if result[:12] == '000000000000':
-                result = result[12:]
-                print 'My Mac is: ' + result[:12]
-                result = result[12:]
-                print 'The Switches MAC is: ' + result[:12]
-                result = result[12:]
-                if result[:8] == transactioncounter:
-                    print 'Is a Reply to my own transaction!'
-                    result = result[8:]
-                    if result[:8] == '00000000':
-                        print 'Is a NSDP reply!'
-                        result = result[16:]
-                        if result[:8] == switchtypestart:
-                            result = result[8:]
-                            length = result.find(switchtypeend)
-                            print 'Switchtype: ' + binascii.unhexlify(result[:length])
-                            result = result[length:]
-        else:
-            print 'got a strange reply: ' + result
+        resultdict = parsedata(sequence,result)
 
-    
     sequence = hex(int(sequence) + 1)[2:].rjust(8,'0')
 
-    discoverydata = constructdata(sequence,'0001000000020000000300000004000000050000000600000007000000080000000b0000000c0000000d0000000e0000000f0000',discoveryheader,macaddress)
+    discoverydata = constructdata(sequence,'0001000000020000000300000004000000050000000600000007000000080000000b0000000c0000000d0000000e0000000f0000',discoveryheader,mymac)
     enddiscoveryresult = send_data(binascii.unhexlify(discoverydata))
-    result = binascii.hexlify(enddiscoveryresult)
-    if result[:4] == discoveryansheader:
-        print 'got a successfull discovery'
-        result = result[4:]
-        if result[:12] == '000000000000':
-            result = result[12:]
-            print 'My Mac should be: ' + result[:12]
-            result = result[12:]
-            print 'The Switches MAC is: ' + result[:12]
-            result = result[12:]
-            if result[:8] == transactioncounter:
-                print 'Is a Reply to my own transaction!'
-                result = result[8:]
-                if result[:8] == '00000000':
-                    print 'IS a NSDP reply!'
-                    result = result[16:]
-                    if result[:8] == switchtypestart:
-                        result = result[8:]
-                        length = result.find(switchtypeend)
-                        print 'Switchtype: ' + binascii.unhexlify(result[:length])
-                        result = result[length:]
+    if enddiscoveryresult != None:
+        result = binascii.hexlify(enddiscoveryresult)
+        resultdict = parsedata(sequence,result)
 
+    if resultdict['packettype'] == discoveryansheader:
+        if switchtype in resultdict:
+            print 'switch type: ' + binascii.unhexlify(resultdict[switchtype])
+        if switchname in resultdict:
+            print 'switch name: ' + binascii.unhexlify(resultdict[switchname])
+        if switchmac in resultdict:
+            print 'switch mac: ' + resultdict[switchmac]
+            destmac = resultdict[switchmac]
+        if switchip in resultdict:
+            print 'switch ip: ' + str(int('0x' + resultdict[switchip][:2],0)) + '.' + str(int('0x' + resultdict[switchip][2:4],0)) + '.' + str(int('0x' + resultdict[switchip][4:6],0)) + '.' + str(int('0x' + resultdict[switchip][6:8],0))
+        if switchnetmask in resultdict:
+            print 'switch netmask: ' + str(int('0x' + resultdict[switchnetmask][:2],0)) + '.' + str(int('0x' + resultdict[switchnetmask][2:4],0)) + '.' + str(int('0x' + resultdict[switchnetmask][4:6],0)) + '.' + str(int('0x' + resultdict[switchnetmask][6:8],0))
+        if switchgateway in resultdict:
+            print 'switch gateway: ' + str(int('0x' + resultdict[switchgateway][:2],0)) + '.' + str(int('0x' + resultdict[switchgateway][2:4],0)) + '.' + str(int('0x' + resultdict[switchgateway][4:6],0)) + '.' + str(int('0x' + resultdict[switchgateway][6:8],0))
+        if switchfirmver in resultdict:
+            print 'switch firmware version: ' + binascii.unhexlify(resultdict[switchfirmver])
     else:
-        print 'got a strange reply: ' + result
-
-
-
+        print 'not our reply!'
 
 def do_authentication():
     authenticateheader = '0103'
