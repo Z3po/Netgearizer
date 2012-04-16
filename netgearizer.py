a2885171accf3baa53266f4315884ad2932cfb25
Sebastian Cabrera <scabrera-github@portachtzig.de>
2012-04-16 23:39:19 +0200

    added do_setSwitchName and do_setPassword functions

--- netgearizer.py
+++ netgearizer.py
@@ -6,6 +6,7 @@ import socket
 import binascii
 import commands
 import re
+import cmd
 from sys import exit
 
 # addressing information
@@ -33,15 +34,16 @@ sequence = '00000000'
 mymac = get_mac()
 destmac = '000000000000'
 unprivilegedrequest = '0101'
-unprivilegedanswer = '0102'
 privilegedrequest = '0103'
 privilegedanswer = '0104'
+password = ''
 
-switchattributes = { 'switch-type' : ('0001','string'), 
-                    'switch-name' : ('0003','string'), 'switch-mac' : ('0004','mac'), 
-                    'switch-ip' : ('0006','ip'), 
-                    'switch-netmask' : ('0007','ip'), 'switch-gateway' : ('0008','ip'), 
-                    'switch-password' : ('000a', 'string'),
+switchattributes = { 'switch-type' : ('0001','string'),
+                    'switch-name' : ('0003','string'), 'switch-mac' : ('0004','mac'),
+                    'switch-ip' : ('0006','ip'),
+                    'switch-netmask' : ('0007','ip'), 'switch-gateway' : ('0008','ip'),
+                    'new-switch-password' : ('0009', 'string'),
+                    'switch-password' : ('000a', 'string'), 'switch-dhcp' : ('000b', 'dhcpoption'),
                     'switch-port-statuses' : ('0c00', 'port-status'),
                     'switch-firmware' : ('000d','string'),
                     'switch-port-counter' : ('1000','port-counter'),
@@ -61,7 +63,7 @@ s.settimeout(0.2)
 # bind to SRCPORT
 s.bind(('',SRCPORT))
 
-def send_data(senddata): # {{{
+def __socketSend(senddata): # {{{
     """Send the data out via SOCKET"""
     # send the data
     s.sendto(senddata,(DESTIP, DESTPORT))
@@ -75,15 +77,23 @@ def send_data(senddata): # {{{
 # }}}
 
 def senddata(reqtype,datalist): # {{{
-    """This function builds the data to send them out via send_data"""
+    """This function builds the data to send them out via __socketSend"""
     increase_sequence()
     nsdpnoerror = '000000000000'
     nsdpseperator = '00000000'
     enddata = 'ffff0000'
     nsdpheader = 'NSDP'.encode('hex')
-    data = reqtype 
+    data = reqtype
+
     data += nsdpnoerror + mymac + destmac 
     data += sequence + nsdpheader + nsdpseperator 
+    if reqtype == privilegedrequest:
+        if len(password) == 0:
+            print 'Please set password first'
+            return None
+        else:
+            data += switchattributes['switch-password'][0] + hex(len(password)/2)[2:].rjust(4,'0')
+            data += password
     if isinstance(datalist[0], tuple):
         for datapair in datalist:
             if len(datapair[1]) < 2:
@@ -93,7 +103,7 @@ def senddata(reqtype,datalist): # {{{
             data += datapair[0] + hex(length)[2:].rjust(4,'0')
             data += datapair[1]
         data += enddata
-        result = send_data(binascii.unhexlify(data))
+        result = __socketSend(binascii.unhexlify(data))
     else:
         if len(datalist[1]) < 2:
             length=0
@@ -102,7 +112,7 @@ def senddata(reqtype,datalist): # {{{
         data += datalist[0] + hex(length)[2:].rjust(4,'0')
         data += datalist[1]
         data += enddata
-        result = send_data(binascii.unhexlify(data))
+        result = __socketSend(binascii.unhexlify(data))
     return result
 # }}}
 
@@ -136,7 +146,7 @@ def parsedata(hexvalue): # {{{
         else:
             print 'Not a result to my sequence!'
     else:
-        dataresult.update({ 'ERROR' : data[7:10] })
+        dataresult.update({ 'ERROR' : data[4:8] })
     return dataresult
 # }}}
 
@@ -157,6 +167,11 @@ def convert_hex(hexvalue, target): # {{{
             result += hexvalue[:2] + ':'
             hexvalue = hexvalue[2:]
         result += hexvalue
+    elif target == 'dhcpoption':
+        if hexvalue == '01':
+            result = 'enabled'
+        else:
+            result = 'disabled'
     elif target == 'port-status':
         result = []
         for port in hexvalue:
@@ -189,7 +204,7 @@ def increase_sequence(): # {{{
     sequence = hex(int(sequence) + 1)[2:].rjust(8,'0')
 # }}}
 
-def print_result(result): # {{{
+def printResult(result): # {{{
     """This function prints the results
     result: hexvalue we get from the switch
     """
@@ -197,14 +212,19 @@ def print_result(result): # {{{
     resultdict = parsedata(result)
 
     if 'ERROR' in resultdict:
-        if resultdict['ERROR'] in switchattributes.key():
-            print 'ERROR with ' + switchattributes[resultdict['ERROR']]
-        print 'ERROR: ' + resultdict['ERROR']
+        found = None
+        for key in  switchattributes.keys():
+            if resultdict['ERROR'] == switchattributes[key][0]:
+                found = key
+        if found != None:
+            print 'ERROR with ' + found
+        else:
+            print 'ERROR: ' + resultdict['ERROR']
     elif resultdict['packettype'] == privilegedanswer:
-        print 'Successfull'
+        print 'Successful'
     else:
         for key in switchattributes.keys():
-            if switchattributes[key][0] in resultdict:
+            if switchattributes[key][0] in resultdict and len(resultdict[switchattributes[key][0]]) > 0:
                 if key == 'switch-mac':
                    destmac = resultdict[switchattributes[key][0]]
                 convertdata = convert_hex(resultdict[switchattributes[key][0]],switchattributes[key][1])
@@ -227,24 +247,26 @@ def do_discovery(): # {{{
     discoveryresult = senddata(unprivilegedrequest,(('0001',''),('0002',''),('0003',''),('0004',''),('0005',''),('0006',''),('0007',''),('0008',''),('000b',''),('000c',''),('000d',''),('000e',''),('000f',''),('7400','')))
 
     discoveryresult = senddata(unprivilegedrequest,(('0001',''),('0002',''),('0003',''),('0004',''),('0005',''),('0006',''),('0007',''),('0008',''),('000b',''),('000c',''),('000d',''),('000e',''),('000f','')))
-    # 
-    print_result(discoveryresult)
+
+    printResult(discoveryresult)
 # }}}
 
-def do_authenticate(password): # {{{
+def do_authenticate(__password): # {{{
     """This function tries to authenticate to the switch
     password : the password to try
     """
+    global password
+    password = __password.encode('hex')
     print 'Authenticating...'
-    authenticationresult = senddata(privilegedrequest,(switchattributes['switch-password'][0],password.encode('hex')))
-    print_result(authenticationresult)
+    result = senddata(privilegedrequest,(switchattributes['switch-password'][0],__password.encode('hex')))
+    printResult(result)
+
 # }}}
 
 def do_getPortCount(): # {{{
     """This function returns the numbers of ports available"""
-
     result = senddata(unprivilegedrequest,((switchattributes['switch-port-count'][0],''),))
-    print_result(result)
+    printResult(result)
 # }}}
 
 def do_getLinkStatus(): # {{{
@@ -252,13 +274,30 @@ def do_getLinkStatus(): # {{{
     linkstatus = '0c00'
     result = senddata(unprivilegedrequest,((linkstatus,''),))
 
-    print_result(result)
+    printResult(result)
 # }}}
 
 def do_getPortCounter(): # {{{
     result = senddata(unprivilegedrequest,('1000',''))
-    print_result(result)
+    printResult(result)
 # }}}
+
+def do_setSwitchName(name): # {{{
+    """This function sets the switch-name
+    name : the name to set
+    """
+    print 'Setting Switch Name...'
+    result = senddata(privilegedrequest,(switchattributes['switch-name'][0],name.encode('hex')))
+    printResult(result)
+# }}}
+
+def do_setPassword(password):
+    """This function changes the password
+    password : the password to set
+    """
+    print 'Setting Password...'
+    result = senddata(privilegedrequest,(switchattributes['new-switch-password'][0],password.encode('hex')))
+    printResult(result)
 
 # MAIN
 do_discovery()
@@ -270,5 +309,8 @@ print '#########'
 do_getLinkStatus()
 print '#########'
 do_getPortCounter()
+print '#########'
+#do_setSwitchName('')
+#do_setPassword('password')
 
 s.close()
